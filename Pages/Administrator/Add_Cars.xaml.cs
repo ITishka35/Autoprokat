c0d4592d98677202d1ac9c30dda5e89dd011ee96
Maria & Vlad <Maria & Vlad@DESKTOP-1P53LV5>
2022-10-16 23:08:09 +0300

    Process

---
+++ Pages/Administrator/Add_Cars.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autoprokat.Pages.Administrator
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_Cars.xaml
+    /// </summary>
+    public partial class Add_Cars : Page
+    {
+        public Add_Cars()
+        {
+            InitializeComponent();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
